84e7e412a1dcf949155b53bc2ff6e9d1c997b98e
Gaston R <gaston.requena@gmail.com>
2023-08-17 11:03:07 -0300

    change: icon

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -5,6 +5,7 @@ import {
 
 import SideBarMobileView from "./MobileView";
 import NavigationItem from "./NavigationItem";
+import Logo from "assets/react.svg";
 
 const navigation = [
   { name: "Ordenes", href: "/", icon: DocumentDuplicateIcon, active: true },
@@ -28,11 +29,7 @@ const SideBar = ({ isOpen, onClose, currentUrl }: Props) => {
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto mr-3"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
+            <img className="h-8 w-auto mr-3" src={Logo} alt="Your Company" />
             Logistic UI
           </div>
           <nav className="flex flex-1 flex-col">
